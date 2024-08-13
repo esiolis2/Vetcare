5765ffb2d96f3f5b2f4e450d9086e27fe461b3aa
esiolis2 <S3935490@student.rmit.edu.au>
2024-08-13 11:25:12 +1000

    git commit "ethan the best"

--- src/main/java/au/edu/rmit/sept/webapp/WebappApplication.java
+++ src/main/java/au/edu/rmit/sept/webapp/WebappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebappApplication {
-
+//ethan the best
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
