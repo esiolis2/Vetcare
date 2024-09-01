6e4d6ee6dcb541d6e165684b530a0a160df08d01
wendyUVo <s3461279@student.rmit.edu.au>
2024-09-01 22:46:56 +1000

    added HomeController.java

--- src/main/java/au/edu/rmit/sept/webapp/controllers/HomeController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controllers/HomeController.java
@@ -1,4 +1,19 @@
 package au.edu.rmit.sept.webapp.controllers;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to Vet Care!");
+        model.addAttribute("today", new Date()); // not a good approach for testing. More later.
+        return "HomePage.html";
+
+    }
+
 }
