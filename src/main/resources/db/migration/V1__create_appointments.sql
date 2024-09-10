cb8e314cf673e2a3a9049e0dcb42131ac80b0dbc
sukhsarao <s3945471@student.rmit.edu.au>
2024-09-10 18:38:33 +1000

    Added Fly way for db migration

---
+++ src/main/resources/db/migration/V1__create_appointments.sql
@@ -0,0 +1,15 @@
+CREATE TABLE APPOINTMENT (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    veterinarianId BIGINT NOT NULL,
+    petOwnerId BIGINT NOT NULL,
+    clinic VARCHAR(255) NOT NULL,
+    ownerName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    appointmentTime DATETIME(6) NOT NULL,
+    reason TEXT,
+    petName VARCHAR(255) DEFAULT NULL,
+    petType VARCHAR(255) DEFAULT NULL,
+    petAge INT DEFAULT NULL,
+    PRIMARY KEY (id)
+);
