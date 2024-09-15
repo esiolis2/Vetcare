0307b3324a6b987f7260dff270ed6e4964ffaae1
Ankita001812 <s3980223@student.rmit.edu.au>
2024-09-16 06:05:11 +1000

    Account creation working fine for this config for me!

--- src/main/java/au/edu/rmit/sept/webapp/repositories/UserRepositoryImpl.java
+++ src/main/java/au/edu/rmit/sept/webapp/repositories/UserRepositoryImpl.java
@@ -1,15 +1,20 @@
 package au.edu.rmit.sept.webapp.repositories;
 
 
-import au.edu.rmit.sept.webapp.models.User;
-import org.springframework.jdbc.datasource.init.UncategorizedScriptException;
-import org.springframework.stereotype.Repository;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import au.edu.rmit.sept.webapp.models.User;
+import org.springframework.jdbc.datasource.init.UncategorizedScriptException;
+import org.springframework.stereotype.Repository;
+import java.sql.Statement;
+
 
 
 @Repository
@@ -20,32 +25,27 @@ public class UserRepositoryImpl implements UserRepository {
         this.source = source;
     }
 
+    @Override
     public User insertUserData(User u){
-      try(
-          Connection connection = this.source.getConnection();
-          PreparedStatement ps = connection.prepareStatement("INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?);")){
+        try(
+                Connection connection = this.source.getConnection();
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO user (name, username, email, password) VALUES (?, ?, ?, ?)")){
 
-          ps.setString(1, u.getName());
-          ps.setString(2, u.getUsername());
-          ps.setString(3, u.getEmail());
-          ps.setString(4, u.getPassword());
+            ps.setString(1, u.getName());
+            ps.setString(2, u.getUsername());
+            ps.setString(3, u.getEmail());
+            ps.setString(4, u.getPassword());
 
-          int rowsAffected = ps.executeUpdate();
-          if (rowsAffected > 0) {
-              // the generated user ID
-              ResultSet generatedKeys = ps.getGeneratedKeys();
-              if (generatedKeys.next()) {
-                  u.setId(generatedKeys.getInt(1));
-              }
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Account successfully created!");
+                return u;
+            }
+        }catch(SQLException e){
+            throw new UncategorizedScriptException("Failed to insert data an user in the database", e);
+        }
 
-              System.out.println("Account successfully created!");
-              return u;
-          }
-      }catch(SQLException e){
-          throw new UncategorizedScriptException("Failed to insert data an user in the database", e);
-      }
-
-      return u;
+        return u;
     }
 
 
