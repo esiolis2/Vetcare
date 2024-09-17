77a67118769620d209d88d64b068dc630ba50b1a
Ankita001812 <s3980223@student.rmit.edu.au>
2024-09-17 15:08:40 +1000

    Bug Fixing Flyway: Half way done

--- src/main/java/au/edu/rmit/sept/webapp/services/UserService.java
+++ src/main/java/au/edu/rmit/sept/webapp/services/UserService.java
@@ -1,7 +1,8 @@
 package au.edu.rmit.sept.webapp.services;
+import  au.edu.rmit.sept.webapp.models.User;
 
 //Interface because it needs follow certain rules or methods
 public interface UserService {
-    public au.edu.rmit.sept.webapp.models.User createUser (au.edu.rmit.sept.webapp.models.User u);
+    public User createUser (User u);
 }
 
