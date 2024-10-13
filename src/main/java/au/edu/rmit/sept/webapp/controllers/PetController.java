52616eb50a0d6ab4f377a685edcac9f5b63e6503
wendyUVo <s3461279@student.rmit.edu.au>
2024-10-13 16:04:17 +1100

    pet management: added Pet Management front end for pet list and add new pet in Dashboard and functionality completed

--- src/main/java/au/edu/rmit/sept/webapp/controllers/PetController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controllers/PetController.java
@@ -12,15 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class PetController {
     private final PetInformationService petInformationService;
     private final UserService userService;
+    private final PetInformationService petInfoService;
 
     @Autowired
-    public PetController(PetInformationService petInformationService, UserService userService) {
+    public PetController(PetInformationService petInformationService, UserService userService, PetInformationService petInfoService) {
         this.petInformationService = petInformationService;
         this.userService = userService;
+        this.petInfoService = petInfoService;
+    }
+
+    @ModelAttribute("loggedInUser")
+    public User getLoggedInUser(HttpServletRequest request) {
+        return (User) request.getSession().getAttribute("loggedInUser");
     }
 
     @GetMapping("/pets/register")
@@ -28,8 +37,9 @@ public class PetController {
         // You can add any additional attributes to the model if needed
         return "PetRegistration"; // This should match the name of your HTML template
     }
+
     @PostMapping("/pets/register")
-    public String registerPet(@ModelAttribute PetInformation petInformation,  HttpServletRequest request) {
+    public String registerPet(@ModelAttribute PetInformation petInformation, HttpServletRequest request) {
         String email = (String) request.getSession().getAttribute("userEmail");
         if (email != null) {
             User user = userService.findByEmail(email);
@@ -39,7 +49,17 @@ public class PetController {
             return "redirect:/appointments"; // Adjust as necessary
 
         }
-        return  "redirect:/login";
+        return "redirect:/login";
 
+    }
+
+    @GetMapping("/dashboard/pet-management")
+    public String petManagementDashboard(Model model, HttpServletRequest request) {
+        String email = (String) request.getSession().getAttribute("userEmail");
+        if (email != null) {
+            List<PetInformation> petInformation = petInfoService.getPetByUserId(userService.findByEmail(email).getId());
+            model.addAttribute("petInformation", petInformation);
+        }
+        return "/dashboard/PetManagement";
     }
 }
